2417de276647fd43796c815a35988dc21c1c8a3e
Chan <dulithaz@gmail.com>
2013-06-20 08:57:16 +0530

    New updates to goose module with support using hash rather than an array

--- modules/goose.js
+++ modules/goose.js
@@ -4,14 +4,28 @@ TODO :- How to handle multipart data
 */
 var goose = (function () {
     var configs = {
-        CONTEXT: "/"
+        CONTEXT: "/",
+		CACHE:false,
+		CACHE_REFRESH:false
     };
     // constructor
-    var routes = new Array();
+	// Will be using a hash rather than an array
+    var routes = {};
 	var log = new Log();
     var module = function (conf) {
         mergeRecursive(configs, conf);
         log.info("Goose Context- " + configs.CONTEXT);
+		if(configs.CACHE){
+			var r = application.get("jaggery.goose.routes");
+			if(r==undefined){
+				application.put("jaggery.goose.routes",routes);
+			}else{
+				routes=r;
+			}
+			route("cacherefresh",function(ctx){
+				application.put("jaggery.goose.routes", undefined);
+			}, "GET");
+		}
     };
 
     function routeOverload(route) {
@@ -38,60 +52,73 @@ var goose = (function () {
     // prototype
     module.prototype = {
         constructor: module,
-        route: function (route, action) {
+        route: function (route, action ,verb) {
             //contains VERB and the route
-            routes.push({
-                    route: routeOverload(route),
-                    action: action
-                });
+			if(configs.CACHE){
+				if(routes[routeOverload(route+"|"+verb)]==undefined){
+					routes[routeOverload(route+"|"+verb)] = {route:routeOverload(route),verb:verb,action:action};
+					log.info("--------Goose CACHE enabled --------" + verb);
+				}
+				return;
+			}
+			log.info("sdfsd");
+			routes[routeOverload(route+"|"+verb)] = {route:route,verb:verb,action:action};
         },
 		get: function (route, action) {
-            this.route(route + "|GET", action);
+            this.route(route, action, "GET");
         },	
 		post: function (route, action) {
-            this.route(route + "|POST", action);
+            this.route(route, action, "POST");
 	    },
 		put: function (route, action) {
-            this.route(route + "|PUT", action);
+            this.route(route, action, "PUT");
 	    },
         process: function (request) {
-			log.info(routes);
-            for (var i = 0; i < routes.length; i++) {
-                var routeObject = routes[i];
-                var routeAction = routeObject.action;
-                var routeVariables = routeObject.route.split("|");
-                var route = routeVariables[0];
-                var verb = routeVariables[1];
-                var uriMatcher = new URIMatcher(request.getRequestURI());
-                if (uriMatcher.match(route)) {
-                    log.info('--------Goose Match--------');
-                }
-                if (uriMatcher.match(route) && request.getMethod() == verb) {
-                    var elements = uriMatcher.elements();
-                    var ctx = elements;
-                    log.info("--------Goose Verb --------" + verb);
-                    log.info("--------Goose Route --------" + route);
-					log.info("--------Goose Elements --------");
-					log.info(elements);
-					var jResult = {};
-					if(verb=="GET"){
-						jResult = request.getAllParameters();
-					}else{
-						jResult = request.getAllParameters();
-						if(request.getContentType()=='application/json'){
-							mergeRecursive(jResult,request.getContent());	
+			for (var property in routes){
+				if(routes.hasOwnProperty(property)){
+					var routeObject = routes[property];
+					log.info(routeObject);
+	                var routeAction = routeObject.action;
+	                var route = routeObject.route;
+	                var verb = routeObject.verb;
+	                var uriMatcher = new URIMatcher(request.getRequestURI());
+					log.info(request.getRequestURI());
+	                if (uriMatcher.match(route)) {
+	                    log.info('--------Goose Match--------');
+	                }
+	                if (uriMatcher.match(route) && request.getMethod() == verb) {
+	                    var elements = uriMatcher.elements();
+	                    var ctx = elements;
+	                    log.info("--------Goose Verb --------" + verb);
+	                    log.info("--------Goose Route --------" + route);
+						log.info("--------Goose Elements --------");
+						log.info(elements);
+						
+						var jResult = {};
+						if(verb=="GET"){
+							jResult = request.getAllParameters();
+						}else{
+							jResult = request.getAllParameters();
+							if(request.getContentType()=='application/json'){
+								mergeRecursive(jResult,request.getContent());	
+							}
 						}
-					}
-					log.info("--------Goose file parsing--------- ");
-					ctx.files = request.getAllFiles();
-					log.info("--------Goose parsed data--------- ");
-					log.info(jResult);
-                    ctx = mergeRecursive(jResult,ctx);
-					log.info("--------Goose final data--------- ");
-					log.info(jResult);
-                    routeAction(ctx);
-                    break;
-                }
+						log.info("--------Goose file parsing--------- ");
+						ctx.files = request.getAllFiles();
+						
+						log.info("--------Goose parsed data--------- ");
+						log.info(jResult);
+	                    ctx = mergeRecursive(jResult,ctx);
+	
+						log.info("--------Goose final data--------- ");
+						log.info(jResult);
+	                    routeAction(ctx);
+	                    break;
+	                }
+				}
+			}
+            for (var i = 0; i < routes.length; i++) {
+                
             }
         }
     };
